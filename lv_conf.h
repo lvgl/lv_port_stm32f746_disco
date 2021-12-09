089db3a0d715f7c98b1d91bb53170bc5d8649d75
Themba Dube <embeddedthemba@gmail.com>
2021-12-09 18:14:10 -0500

    Keep PERF_MONITOR and MEM_MONITOR enabled

--- lv_conf.h
+++ lv_conf.h
@@ -188,11 +188,11 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
  *-----------*/
 
 /*1: Show CPU usage and FPS count in the right bottom corner*/
-#define LV_USE_PERF_MONITOR     0
+#define LV_USE_PERF_MONITOR     1
 
 /*1: Show the used memory and the memory fragmentation  in the left bottom corner
  * Requires LV_MEM_CUSTOM = 0*/
-#define LV_USE_MEM_MONITOR      0
+#define LV_USE_MEM_MONITOR      1
 
 /*1: Draw random colored rectangles over the redrawn areas*/
 #define LV_USE_REFR_DEBUG       0
